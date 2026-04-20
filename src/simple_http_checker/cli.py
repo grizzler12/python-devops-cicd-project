efd986d50f6eeeb6005f39be9bfd4fd32f71d369
grizzler12 <sljn.alexandru@yahoo.com>
2026-04-20 18:14:49 +0300

    fixed cli.py

--- src/simple_http_checker/cli.py
+++ src/simple_http_checker/cli.py
@@ -3,8 +3,8 @@ import click
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(asctime)s] %[levelname]-8s %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
+    format="[%(asctime)s] %(levelname)-8s %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
