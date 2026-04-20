1f78f94868378070e4ed77e30b104c2589c979f5
grizzler12 <sljn.alexandru@yahoo.com>
2026-04-20 17:34:27 +0300

    better formated checker.py

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -1,7 +1,8 @@
-import logging 
+import logging
 import requests
 
 logger = logging.getLogger(__name__)
+
 
 def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
     """
@@ -14,17 +15,17 @@ def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
     Returns:
         A dictionary mapping each URL to its status string.
     """
-    
+
     logger.info(f"Starting check for {len(urls)} URLs with a timeout of {timeout}")
     results: dict[str, str] = {}
-    
-    for url in urls: 
-        status = "UNKNOWN" 
-        
-        try: 
+
+    for url in urls:
+        status = "UNKNOWN"
+
+        try:
             logger.debug(f"Checking URL: {url}")
             response = requests.get(url, timeout=timeout)
-            
+
             if response.ok:
                 status = f"{response.status_code} OK"
             else:
@@ -37,10 +38,12 @@ def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
             logger.warning(f"Connection error for {url}")
         except requests.exceptions.RequestException as e:
             status = f"REQUEST_ERROR: {type(e).__name__}"
-            logger.error(f"An unexpected request error occured for {url}: {e}", exc_info=True)
-            
+            logger.error(
+                f"An unexpected request error occured for {url}: {e}", exc_info=True
+            )
+
         results[url] = status
         logger.debug(f"Checked: {url:<40} -> {status}")
-    
+
     logger.info("URL check finished.")
-    return results 
+    return results
