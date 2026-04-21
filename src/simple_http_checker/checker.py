3a6625711910b95d804d9269def5ae67a3437c0c
grizzler12 <sljn.alexandru@yahoo.com>
2026-04-21 18:14:33 +0300

    removed API_TOKEN - security check failed as expected

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -4,7 +4,6 @@ from typing import Collection
 
 logger = logging.getLogger(__name__)
 
-API_TOKEN = "ghp_ThisLooksLikeAToken"  # to be flagged by bandit, test purpose
 
 
 def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
